19b93337e0c07a577c90eb91dbb45dca4dc632d5
Zack Jackson <z.jackson58@gmail.com>
2018-08-13 21:57:06 -0400

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require("gulp");
+var browserSync = require("browser-sync").create();
+
+gulp.task("serve", function() {
+  browserSync.init({
+    server: "./"
+  });
+
+  gulp.watch("./*.html").on("change", browserSync.reload);
+});
+
+gulp.task("default", ["serve"]);
