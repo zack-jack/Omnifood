d47e6ac236967b673051a2aa6651718a549bde85
Zack Jackson <z.jackson58@gmail.com>
2018-08-17 20:02:42 -0400

    mobile view responsive nav with jquery

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -103,4 +103,20 @@ $(document).ready(function() {
       offset: "40%"
     }
   );
+
+  /* Mobile nav links */
+  $(".js--mobile-nav-icon").click(function() {
+    var nav = $(".js--main-nav");
+    var icon = $(".js--mobile-nav-icon i");
+
+    nav.slideToggle(200);
+
+    if (icon.hasClass("ion-navicon-round")) {
+      icon.addClass("ion-close-round");
+      icon.removeClass("ion-navicon-round");
+    } else {
+      icon.addClass("ion-navicon-round");
+      icon.removeClass("ion-close-round");
+    }
+  });
 });
